411b35586b650ee5f21f946a064b8a2ff0e0236c
Chris Fenos <chris.m.fenos@gmail.com>
2018-07-07 19:44:04 -0400

    contacts have been saved to state from cli in the contacts.js page

--- web/src/components/Contacts.js
+++ web/src/components/Contacts.js
@@ -1,31 +1,78 @@
 import React, { Component } from 'react'
-import { Button, Checkbox, Form } from 'semantic-ui-react'
+import { Button, Checkbox, Form, List } from 'semantic-ui-react'
+import axios from 'axios';
 
 class Contacts extends Component {
-
-    componentDidMount = () => {
-        axios.get('http://localhost:3210/wallets')
-        .then(function(response){
-            console.log(response.data);
-        }); 
-    }
 
     constructor(props) {
         super(props);
         this.state = {
-        contacts: [] 
+            contacts: [],
+            loading: false
+        }
+
     }
-  }
+
+    componentDidMount = () => {      
+        this.getContacts();
+    }
 
     getContacts() {
-      console.log(this.state.contacts)
-     }
+      //console.log(this.state.contacts)    
+      var x = axios.get('http://localhost:3210/wallets')
+      .then((response) => {          
+          this.setState({contacts: response.data}) 
+          this.setState({loading: true}) 
+          return response.data; 
+      });
+      return x;
+    }
 
-  
+
     render() {
+
+        const items = this.state.contacts;
+        // console.log(items);
+
+     
+        // const ListExampleFloated = () => (
+        //     <List divided verticalAlign='middle'>
+        //       <List.Item>
+        //         <List.Content floated='right'>
+        //           <Button>Add</Button>
+        //         </List.Content>          
+        //         <List.Content>Lena</List.Content>
+        //       </List.Item>
+        //       <List.Item>
+        //         <List.Content floated='right'>
+        //           <Button>Add</Button>
+        //         </List.Content>             
+        //         <List.Content>Lindsay</List.Content>
+        //       </List.Item>
+        //       <List.Item>
+        //         <List.Content floated='right'>
+        //           <Button>Add</Button>
+        //         </List.Content>               
+        //         <List.Content>Mark</List.Content>
+        //       </List.Item>
+        //       <List.Item>
+        //         <List.Content floated='right'>
+        //           <Button>Add</Button>
+        //         </List.Content>       
+        //         <List.Content>Molly</List.Content>
+        //       </List.Item>
+        //     </List>
+        //   )
+
+
       return (
         <div>
-            <p> in the contacts </p>
+             <p> in the contacts </p>   
+
+            {
+                  this.state.loading ? <p> stuff is loaded </p> : <p> content is not yet loaded </p>
+            }
+                  
         </div>
       );
     }
