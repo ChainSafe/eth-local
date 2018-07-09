408eddc3564dd3ea6201c1256fd6cd4488d97114
David Ansermino <david@chainsafe.io>
2018-07-09 13:02:28 -0400

    Added styling and new build

--- client/ui/src/index.js
+++ client/ui/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'semantic-ui-react/'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
